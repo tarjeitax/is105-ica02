08a43cf0fed7fbb0d12a0b1758d7bcc9bbeab19a
JanisJanis <janis.gailis@uia.no>
2017-02-08 10:27:41 +0100

    Initiell fil for ICA02 deloppgave i Oppgave 2

---
+++ sum/sum.go
@@ -0,0 +1,9 @@
+package sum
+
+func SumInt8(a, b int8) int8 {
+	return a + b
+}
+
+func SumFloat64(a, b float64) float64 {
+	return a + b
+}
---
+++ sum/sum_test.go
@@ -0,0 +1,22 @@
+package sum
+
+import "testing"
+
+// Check https://golang.org/ref/spec#Numeric_types and stress the limits!
+var sum_tests_int8 = []struct {
+	n1       int8
+	n2       int8
+	expected int8
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{127, 1, 128},
+}
+
+func TestSumInt8(t *testing.T) {
+	for _, v := range sum_tests_int8 {
+		if val := SumInt8(v.n1, v.n2); val != v.expected {	
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
