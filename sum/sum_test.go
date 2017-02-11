8344f9dce13c0473a68bc8174b93218c9f33e9e2
JanisJanis <janis.gailis@uia.no>
2017-02-11 12:15:05 +0100

    Boring boring boring ...

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -10,12 +10,12 @@ var sum_tests_int8 = []struct {
 }{
 	{1, 2, 3},
 	{4, 5, 9},
-	{127, 1, 128},
+	{120, 1, 119},
 }
 
 func TestSumInt8(t *testing.T) {
 	for _, v := range sum_tests_int8 {
-		if val := SumInt8(v.n1, v.n2); val != v.expected {	
+		if val := SumInt8(v.n1, v.n2); val != v.expected {
 			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
 		}
 	}
